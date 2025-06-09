950b57519bc21d8e600bf72d06e7b8678465cdbe
Damian X <dx4336969@gmail.com>
2025-06-09 23:46:46 +0000

    style: add navbar to home

---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,22 @@
+function Navbar() {
+  return (
+    <nav className=" flex justify-between  px-20 w-full items-center mt-10 text-white">
+      <div>
+        <h1 className="text-xl font-bold ">WunkatHomes</h1>
+      </div>
+
+      <ul className="flex  items-center justify-between
+backdrop-blur-3xl overflow-hidden rounded-2xl   text-lg ">
+  
+        <li className=" px-4 rounded-r-2xl py-2">Home</li>
+        <li className="nav-item">Buy</li>
+        <li className="nav-item">Rent</li>
+        <li className="nav-item">About</li>
+        <li className="nav-item">Contact us</li>
+      </ul>
+      <div className="bg-[#000] rounded-lg text-lg px-4 py-1 font-extrabold"><span>login</span></div>
+    </nav>
+  )
+}
+
+export default Navbar
