4f85b9b99dd5602032608fa34b64b29760916ab2
Damian X <dx4336969@gmail.com>
2025-06-24 15:29:29 +0000

    feat: add login page /login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image"
 import { Input } from "../_components/input"
+import { Checkbox } from "../_components/checkbox"
+import { FaGoogle } from "react-icons/fa"
+import Button from "../_components/Button"
 
 function Login() {
   return (
 
     <div className="grid grid-cols-2 h-[35rem] m-20 justify-center items-center shadow border-gray-200  ">
       <div className="flex flex-col
-       px-36">
+       px-32">
 
       <div className="space-y-2">
 <h1 className="font-bold text-2xl font-karla">Welcome back</h1>
 <h3 className="text-sm text-gray-500">Welcome back! Please enter your details.</h3>
       </div>
-      <div className="flex flex-col gap-4 mt-6">
+      <div className="flex flex-col gap-4 mt-6 ">
         <label>
           <p className="text-sm font-semibold font-karla">Email</p>
 
@@ -23,6 +26,26 @@ function Login() {
 
         <Input type="password" placeholder="Enter your password"/>
         </label>
+      </div>
+      <div className="flex  justify-between text-xs items-center pt-4 font-medium font-karla">
+        <div className="flex items-center  gap-1">
+
+        <Checkbox/>
+        <span>Remember for 30 days</span>
+        </div>
+        <p>Forgot password</p>
+        
+      </div>
+      <div className="flex flex-col pt-4 space-y-3">
+        <Button className="bg-black text-white font-karla font-medium py-1 rounded-lg">
+          Sign in 
+        </Button>
+        <Button className="border text-gray-700 flex items-center gap-2 justify-center font-karla font-medium py-1 rounded-lg">
+         <FaGoogle/><span> Sign in with Google </span>
+        </Button>
+      </div>
+      <div className="pt-8 text-xs">
+        <p><span className="text-gray-500 font-karla text-sm">Don't have an account? </span><span className="font-bold tracking-tighter pl-1 font-karla relative">Sign up for free  <img src="images/und.png" className="w-20 absolute right-0" alt=""  /></span></p>
       </div>
       </div>
       <div className="relative size-full">
