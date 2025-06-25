fa6c60186f5285c8fe00a8d39599e1d77297f8e6
Damian X <dx4336969@gmail.com>
2025-06-25 11:17:36 +0000

    feat: add sign up page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,59 +4,57 @@ import { Checkbox } from "../_components/checkbox";
 import { FaGoogle } from "react-icons/fa";
 import Button from "../_components/Button";
 
-function Login() {
+function Signup() {
   return (
-    <div className="sm:grid grid-cols-2 mx-6 mt-24 sm:h-[35rem] sm:m-20 justify-center items-center border shadow border-gray-200   ">
-      <div
-        className="flex flex-col
-       sm:px-32 px-6 py-10 border-gray-500 "
-      >
+    <div className="sm:grid grid-cols-2 mx-6 sm:mt-24 my-10 sm:h-[35rem] sm:m-20 justify-center items-center border shadow border-gray-200">
+      <div className="flex flex-col sm:px-32 px-6 py-10 border-gray-500">
         <div className="space-y-2">
           <h1 className="font-bold text-3xl sm:text-2xl font-karla">
-            Welcome back
+            Create an account
           </h1>
           <h3 className="sm:text-sm text-base text-gray-500 leading-5">
-            Welcome back! Please enter your details.
+            Let’s get you started with your new account.
           </h3>
         </div>
-        <div className="flex flex-col gap-4 mt-6 ">
+        <div className="flex flex-col gap-4 mt-6">
+          <label className="space-y-1 sm:space-y-0">
+            <p className="sm:text-sm font-semibold font-karla text-base">Name</p>
+            <Input type="text" placeholder="Enter your full name" className="py-5 sm:py-1" />
+          </label>
           <label className="space-y-1 sm:space-y-0">
             <p className="sm:text-sm font-semibold font-karla text-base">Email</p>
-
-            <Input type="text" placeholder="Enter your email" className="py-5 sm:py-1" />
+            <Input type="email" placeholder="Enter your email" className="py-5 sm:py-1" />
           </label>
           <label className="space-y-1 sm:space-y-0">
             <p className="sm:text-sm font-semibold font-karla text-base">Password</p>
-
-            <Input type="password" placeholder="Enter your password" className="py-5 sm:py-1"/>
+            <Input type="password" placeholder="Create a password" className="py-5 sm:py-1" />
           </label>
         </div>
-        <div className="flex  justify-between text-sm sm:text-xs items-center pt-5 font-medium font-karla">
-          <div className="flex items-center  gap-1">
+        <div className="flex justify-between text-sm sm:text-xs items-center pt-5 font-medium font-karla">
+          <div className="flex items-center gap-1">
             <Checkbox />
-            <span>Remember for 30 days</span>
+            <span>I agree to the terms and conditions</span>
           </div>
-          <p>Forgot password</p>
         </div>
         <div className="flex flex-col pt-4 space-y-3">
           <Button className="bg-black text-white font-karla font-medium py-2 sm:py-1 rounded-lg">
-            Sign in
+            Create Account
           </Button>
           <Button className="border text-gray-700 flex items-center gap-2 justify-center font-karla font-medium py-2 sm:py-1 rounded-lg">
             <FaGoogle />
-            <span> Sign in with Google </span>
+            <span> Sign up with Google </span>
           </Button>
         </div>
         <div className="pt-8 text-xs">
           <p>
             <span className="text-gray-500 font-karla text-sm">
-              Don&apos;t have an account?{" "}
+              Already have an account?
             </span>
-            <span className="font-bold tracking-tighter pl-1 font-karla relative">
-              Sign up for free{" "}
+            <span className="font-bold tracking-tighter pl-1 inline-block w-20 font-karla relative  ">
+              Sign in
               <img
                 src="images/und.png"
-                className="w-20 absolute right-0"
+                className="w-12  absolute right-9"
                 alt=""
               />
             </span>
@@ -65,8 +63,8 @@ function Login() {
       </div>
       <div className="relative hidden sm:block size-full">
         <Image
-          src="/images/img-6.jpg"
-          alt="Login Image"
+          src="/images/img-5.jpg"
+          alt="Signup Image"
           fill
           className="object-cover w-full h-full"
         />
@@ -75,4 +73,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
