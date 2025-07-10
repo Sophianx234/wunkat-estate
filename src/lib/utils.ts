26dbf33b868fc267cf5f84990b86f2c5b94330eb
sophianx234 <dx4336969@gmail.com>
2025-07-10 17:22:12 +0000

    fix: login not working in production

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,7 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL || "http://localhost:3000" 
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL || "http://localhost:3000/api"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
