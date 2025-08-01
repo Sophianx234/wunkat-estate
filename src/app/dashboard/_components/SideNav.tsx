4ad3cca6ca455a185323f22b283c7ae3869b869d
sophianx234 <dx4336969@gmail.com>
2025-08-01 10:18:33 +0000

    feat: make settings page responsive

--- src/app/dashboard/_components/SideNav.tsx
+++ src/app/dashboard/_components/SideNav.tsx
@@ -56,21 +56,22 @@ export default function Sidebar({ handleClose, type = 'normal' }: sidebarProps) 
       animate={{ x: 0 }}
       exit={{ x: "-100%" }}
       transition={{ duration: 0.3 }}
-      className={`w-64 ${type === 'normal' ? 'hidden sm:fixed sm:block left-0 top-0  h-screen w-64 bg-white shadow z-50' : 'block absolute h-dvh top-0 left-0 z-10'} sm:block bg-white shadow pb-6 `}
+      className={`w-64 ${type === 'normal' ? 'hidden sm:fixed sm:block left-0 top-0  h-screen w-64 bg-white shadow z-50' : 'block absolute h-dvh top-0 left-0 -z-20 md:z-10'} sm:block bg-white md:shadow pb-6 `}
     >
       {/* Wrapper with flex column layout */}
-      <div className="flex flex-col h-dvh">
+      <div className="flex flex-col h-full md:h-dvh">
         
         {/* Top section: Logo and close button */}
-        <div className="flex items-center justify-between mb-5 pt-6 border-b border-b-gray-200 pr-2 pb-5 pl-5 border-r border-r-gray-200">
+        <div className="md:flex hidden  items-center justify-between mb-5 pt-6 border-b border-b-gray-200 pr-2 pb-5 pl-5 border-r border-r-gray-200">
           <Logo type="dash" />
           {type === 'slide' && (
             <X size={30} onClick={handleClose} className="block self-end stroke-black cursor-pointer" />
           )}
         </div>
+        <div className="md:hidden flex mb-5 pt-16 bg-transparent"></div>
 
         {/* Scrollable nav container */}
-        <nav className="flex-1 overflow-y-auto scrollbar-hide  text-sm font-medium text-gray-600 px-4 space-y-1">
+        <nav className="flex-1 overflow-y-auto shadow md:shadow-none border md:border-none border-gray-200 scrollbar-hide  text-sm font-medium text-gray-600 px-4 space-y-1">
          <Link href="/dashboard/finance/analytics" className="dash-nav-item">
   <TbReportAnalytics className="size-6" />
   Financial Analytics
