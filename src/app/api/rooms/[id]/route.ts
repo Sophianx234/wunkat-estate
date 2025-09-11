ecc3b2188f065bf70ee0f0cb407e0b515ca6f092
sophianx234 <dx4336969@gmail.com>
2025-09-11 08:28:29 +0000

    fix: clean errors

--- src/app/api/rooms/[id]/route.ts
+++ src/app/api/rooms/[id]/route.ts
@@ -2,21 +2,33 @@ import { connectToDatabase } from "@/config/DbConnect";
 import Room from "@/models/Room";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest,context:{params:{id:string}}){
-  try{
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    await connectToDatabase();
 
-    await connectToDatabase()
-    const {id} = await context.params
-    console.log('idxxx',id)
-    if(!id) return NextResponse.json({msg:'could not find ID:'},{status:400})
-      const room = await Room.findById(id)
-    if(!room){
-      return NextResponse.json({msg:'could not fetch room with specified ID:'},{status:400})
+    const { id } = params;
+    console.log("idxxx", id);
+
+    if (!id) {
+      return NextResponse.json(
+        { msg: "could not find ID:" },
+        { status: 400 }
+      );
     }
-    return NextResponse.json(room,{status:200})
-  }catch(err){
-   return  NextResponse.json({err},{status:500})
-  }
-  
 
+    const room = await Room.findById(id);
+    if (!room) {
+      return NextResponse.json(
+        { msg: "could not fetch room with specified ID:" },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(room, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ err }, { status: 500 });
+  }
 }
